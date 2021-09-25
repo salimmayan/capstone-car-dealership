7000690eddfd113c5dfd58e7cabe0935006ea719
Salim Mayan <mailsalim@gmail.com>
2021-09-25 11:36:01 -0700

    working boiler plate react application

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
+import Header from "./Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+// const HelpQueueHeader = styled.h1`
+//   font-size: 24px;
+//   text-align: center;
+//   color: blue;
+// `;
+
+function Header(){
+  return (
+    <React.Fragment>
+        {/* <HelpQueueHeader>
+          Help Queue2
+        </HelpQueueHeader>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+        </ul> */}
+        <h1>React Application</h1>
+    </React.Fragment>
+  );
+}
+
+export default Header;
