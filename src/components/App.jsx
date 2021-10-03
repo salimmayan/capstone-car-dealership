5bb63412ff986be046f0037038af3d3f81c9c842
Salim Mayan <mailsalim@gmail.com>
2021-10-03 16:56:16 -0700

    add new footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,8 +37,9 @@ function App() {
           </Route> */}
         </Switch>
       </main>
-      <FooterPage />
+     
     </div>
+    <FooterPage />
   </Router>
   );
 }
--- src/components/FooterPage.jsx
+++ src/components/FooterPage.jsx
@@ -1,51 +1,62 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import "./Car.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube, faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-const FooterPage = () => {
+function Footer() {
   return (
-    <div className="footer">
-      <MDBFooter color="blue" className="font-small pt-4 mt-4">
-        <MDBContainer fluid className="text-center text-md-left">
-          <MDBRow>
-            <MDBCol md="6">
-              <h6 className="title">CONSOLIDATED CARS</h6>
-              <p className="footerAlt">
-                © 2021 CONSOLIDATED CARS. ALL RIGHTS RESERVED. 
-              </p>
-              <a href="https://www.facebook.com/dealeron/" target="_blank" rel="noopener noreferrer" className = "padding fa fa-facebook"> </a> 
-              <a href="https://twitter.com/DealerOn" target="_blank" rel="noopener noreferrer" className = "padding fa fa-twitter"> </a>
-              <a href="https://www.instagram.com/dealeron_inc/" target="_blank" rel="noopener noreferrer" className = "padding fa fa-instagram"> </a>
-            </MDBCol>
-            <MDBCol md="3">
-              <h6 className="title">About</h6>
-              <ul>
-                <li className="list-unstyled">
-                  <p className="footerAlt">Terms of Use</p>
-                </li>
-                <li className="list-unstyled">
-                  <p className="footerAlt">Privacy</p>
-                </li>
-              </ul>
-            </MDBCol>
-            <MDBCol md="3">
-              <h6 className="title">Contact</h6>
-              <ul>
-                <li className="list-unstyled">
-                  <p className="footerAlt">(800) 450 3124</p>
-                </li>
-              </ul>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-        {/* <div className="footer-copyright text-center py-3">
-          <MDBContainer fluid>
-            &copy; {new Date().getFullYear()} Copyright:{" "}
-            <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
-          </MDBContainer>
-        </div> */}
-      </MDBFooter>
+    <div className="main-footer">
+      <div className="containerNew">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col">
+            <h6 className="title">CONSOLIDATED CARS</h6>
+            <h1 className="list-unstyled"></h1>
+            <ul >
+
+             <a href="https://www.youtube.com/user/DealerOnVideo" className="youtube social"> <FontAwesomeIcon icon={faYoutube} size="2x" /> </a> <br></br>
+               <a href="https://www.facebook.com/dealeron/" className="facebook social"> <FontAwesomeIcon icon={faFacebook} size="2x" /> </a><br></br>
+             <a href="https://twitter.com/DealerOn" className="twitter social"> <FontAwesomeIcon icon={faTwitter} size="2x" /> </a><br></br>
+              <a href="https://www.instagram.com/dealeron_inc/" className="instagram social"> <FontAwesomeIcon icon={faInstagram} size="2x" /></a><br></br>
+
+
+              {/* <li><a href="https://www.facebook.com/dealeron/" target="_blank" rel="noopener noreferrer" className="padding fa fa-facebook anchorTagColor"> </a>   </li>
+              <li> <a href="https://twitter.com/DealerOn" target="_blank" rel="noopener noreferrer" className="padding fa fa-twitter anchorTagColor"> </a> </li>
+              <li>  <a href="https://www.instagram.com/dealeron_inc/" target="_blank" rel="noopener noreferrer" className="padding fa fa-instagram anchorTagColor"> </a> </li> */}
+            </ul>
+          </div>
+          {/* Column2 */}
+          <div className="col">
+            <h6 className="title">About</h6>
+            <ul>
+              <li className="list-unstyled">
+                <p className="footerAlt">Terms of Use</p>
+              </li>
+              <li className="list-unstyled">
+                <p className="footerAlt">Privacy</p>
+              </li>
+            </ul>
+          </div>
+          {/* Column3 */}
+          <div className="col">
+            <h6 className="title">Contact</h6>
+            <ul>
+              <li className="list-unstyled">
+                <p className="footerAlt">(800) 450 3124</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} CONSOLIDATED CARS | All rights reserved |
+            Terms Of Service | Privacy
+          </p>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-export default FooterPage;
+export default Footer;
